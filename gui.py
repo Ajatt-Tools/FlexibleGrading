b55a0fb52b770b00ead3acc4a7aa96df435de6e5
Ren Tatsumoto <tatsu@autistici.org>
2021-05-04 20:45:10 +0300

    move consts

--- gui.py
+++ gui.py
@@ -25,31 +25,7 @@ from aqt.qt import *
 from aqt.utils import openLink
 
 from .config import config
-
-ADDON_NAME = "AJT Flexible Grading"
-GITHUB = "https://github.com/Ajatt-Tools"
-STYLING = """
-<style>
-a { color: SteelBlue; }
-h2 { text-align: center; }
-</style>
-"""
-ABOUT_MSG = STYLING + f"""\
-<h2>{ADDON_NAME}</h2>
-<p>An <a href="{GITHUB}">Ajatt-Tools</a> add-on for fast, smooth and efficient reviewing.</p>
-<p>Copyright © Ren Tatsumoto</p>
-<p>
-This software is licensed under <a href="{GITHUB}/FlexibleGrading/blob/main/LICENSE">GNU AGPL version 3</a>.
-Source code is available on <a href="{GITHUB}/FlexibleGrading">GitHub</a>.
-</p>
-<p>For the list of colors, see <a href="https://www.w3schools.com/colors/colors_groups.asp">w3schools.com</a>.</p>
-<p>
-A big thanks to all the people who donated to make this project possible
-and <a href="https://www.youtube.com/c/MATTvsJapan">mattvsjapan</a> for the idea for this add-on.
-</p>
-"""
-DONATE_LINK = "https://tatsumoto-ren.github.io/blog/donating-to-tatsumoto.html"
-COMMUNITY_LINK = "https://tatsumoto-ren.github.io/blog/join-our-community.html"
+from .consts import *
 
 
 def make_color_line_edits() -> Dict[str, QLineEdit]:
