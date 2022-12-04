baf7932bc358ec4624e110523cec174da88167d2
Ren Tatsumoto <tatsu@autistici.org>
2022-12-04 21:21:06 +0300

    properly disable key shortcuts in passfail mode if key was changed in config

--- vim_shortcuts.py
+++ vim_shortcuts.py
@@ -2,7 +2,7 @@
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 import functools
-from typing import Callable, Literal, cast, List, Tuple
+from typing import Callable, Literal, cast, List, Tuple, Iterable
 
 from anki.hooks import wrap
 from aqt.reviewer import Reviewer
@@ -25,45 +25,60 @@ def answer_card(self: Reviewer, grade: str):
         raise RuntimeError("Flexible grading error: Couldn't answer card due to a bug in Anki.") from e
 
 
+def enabled_answer_buttons() -> Iterable[str]:
+    # In PassFail mode pressing 'Hard' and 'Easy' is not allowed.
+    return ('again', 'good') if config['pass_fail'] is True else ('again', 'hard', 'good', 'easy')
+
+
+def enabled_number_keys() -> Iterable[str]:
+    # In PassFail mode pressing 'Hard' and 'Easy' is not allowed.
+    return ('1', '3') if config['pass_fail'] is True else ('1', '2', '3', '4')
+
+
+def number_shortcuts(self: Reviewer):
+    return [
+        (key, func)
+        for key, func
+        in [
+            ("1", lambda: answer_card(self, grade='again')),
+            ("2", lambda: answer_card(self, grade='hard')),
+            ("3", lambda: answer_card(self, grade='good')),
+            ("4", lambda: answer_card(self, grade='easy')),
+        ]
+        if key in enabled_number_keys()
+    ]
+
+
 def new_shortcuts(self: Reviewer) -> List[Tuple[str, Callable]]:
     return [
-        ("1", lambda: answer_card(self, grade='again')),
-        ("2", lambda: answer_card(self, grade='hard')),
-        ("3", lambda: answer_card(self, grade='good')),
-        ("4", lambda: answer_card(self, grade='easy')),
-
+        *number_shortcuts(self),
         *[
             (config.get_key(answer), functools.partial(answer_card, self, grade=answer))
-            for answer in ('again', 'hard', 'good', 'easy')
+            for answer in enabled_answer_buttons()
         ],
-
         (config.get_key("undo"), self.mw.undo),
         (config.get_key("last_card"), LastEase.open_last_card),
     ]
+
+
+def old_shortcuts(self: Reviewer, _old: Callable[[Reviewer], List]) -> List[Tuple[str, Callable]]:
+    # Filter out default number-keys.
+    return [(key, func) for key, func in _old(self) if key not in ('1', '2', '3', '4',)]
 
 
 def is_key_set(shortcut_key: Tuple[str, Callable]) -> bool:
     return bool(shortcut_key[0])
 
 
-def old_shortcuts(self: Reviewer, _old: Callable[[Reviewer], List]) -> List[Tuple[str, Callable]]:
-    return [(k, v) for k, v in _old(self) if k not in ('1', '2', '3', '4',)]
-
-
 def add_vim_shortcuts(self: Reviewer, _old: Callable[[Reviewer], List]) -> List[Tuple[str, Callable]]:
     # Credit: https://ankiweb.net/shared/info/1197299782
-    shortcuts = list(dict([
+    return list(dict([
         *old_shortcuts(self, _old),
-        *filter(is_key_set, new_shortcuts(self))
+        *filter(is_key_set, new_shortcuts(self)),
     ]).items())
-
-    if config['pass_fail'] is True:
-        # PassFail mode. Pressing 'Hard' and 'Easy' is not allowed.
-        return [(k, v) for k, v in shortcuts if k not in ('j', 'l', '2', '4',)]
-    else:
-        return shortcuts
 
 
 def main():
     # Add vim answer shortcuts
+    # noinspection PyProtectedMember
     Reviewer._shortcutKeys = wrap(Reviewer._shortcutKeys, add_vim_shortcuts, "around")
