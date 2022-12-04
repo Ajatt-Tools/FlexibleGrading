045191efafc70f575dabaa40122ffa951f92bef0
Ren Tatsumoto <tatsu@autistici.org>
2022-12-04 04:05:40 +0300

    store color keys as lowercase

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Copyright: Ren Tatsumoto <tatsu at autistici.org>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-from typing import Dict, Iterable
+from typing import Dict, Iterable, Union, overload
 
 from aqt import mw
 
@@ -18,16 +18,23 @@ class ConfigManager:
         return mw.addonManager.getConfig(__name__)
 
     def __init__(self, default: bool = False):
-        self._config = self._get_config() if not default else self._get_default_config()
-        self._default = default
+        self._default_config = self._get_default_config()
+        self._config = self._default_config if default else self._get_config()
+
+    def _get(self, key: str):
+        return self._config.get(key, self._default_config[key])
 
     def __getitem__(self, key: str) -> bool:
-        assert type(self._config[key]) == bool
-        return self._config[key]
+        assert type(val := self._get(key)) == bool
+        return val
 
     def __setitem__(self, key, value):
-        assert type(self._config[key]) == bool
+        assert type(self._get(key)) == bool
         self._config[key] = value
+
+    @property
+    def default(self) -> bool:
+        return self._default_config is self._config
 
     @staticmethod
     def get_label(ease: int, default_ease: int = 3) -> str:
@@ -41,19 +48,37 @@ class ConfigManager:
             return "Easy"
         return "Unknown"
 
-    def get_color(self, ease: int, default_ease: int = 3) -> str:
-        return self._config['colors'][self.get_label(ease, default_ease)]
+    @overload
+    def get_color(self, ease: int, default_ease: int) -> str:
+        ...
+
+    @overload
+    def get_color(self, label: str) -> str:
+        ...
+
+    def get_color(self, ease_or_label: Union[int, str], default_ease: int = 3) -> str:
+        label = (
+            ease_or_label
+            if type(ease_or_label) == str
+            else self.get_label(ease_or_label, default_ease)
+        ).lower()
+        return self._config['colors'].get(label, self._default_config['colors'].get(label, "Pink"))
 
     @property
     def colors(self) -> Dict[str, str]:
         """Returns a dict mapping buttons' labels to their colors."""
-        return dict(self._config['colors'])
+        return {
+            label.lower(): self._config['colors'].get(label.lower(), color_text)
+            for label, color_text in
+            self._default_config['colors'].items()
+        }
 
     def get_toggleables(self) -> Iterable[str]:
-        return (key for key, value in self._config.items() if isinstance(value, bool))
+        """Returns an iterable of boolean keys in the config."""
+        return (key for key, value in self._default_config.items() if isinstance(value, bool))
 
     def set_color(self, btn_label: str, color: str):
-        self._config['colors'][btn_label] = color
+        self._config['colors'][btn_label.lower()] = color
 
     def get_zoom_state(self, state: str) -> float:
         return self._config.setdefault('zoom_states', {}).get(state, 1)
@@ -62,7 +87,7 @@ class ConfigManager:
         self._config.setdefault('zoom_states', {})[state] = value
 
     def write_config(self):
-        if self._default:
+        if self.default:
             raise RuntimeError("Can't write default config.")
         mw.addonManager.writeConfig(__name__, self._config)
 
