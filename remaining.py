c89af9396578a2b90abbc901a94da5e60841e4d5
Ren Tatsumoto <tatsu@autistici.org>
2022-12-06 07:20:49 +0300

    remaining: skip values that don't convert to int

--- remaining.py
+++ remaining.py
@@ -2,7 +2,7 @@
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 import re
-from typing import Callable
+from typing import Callable, Optional
 
 from anki.hooks import wrap
 from aqt.reviewer import Reviewer
@@ -16,13 +16,19 @@ def strip_html_tags(s: str) -> str:
     return re.sub(HTML_TAG, '', s)
 
 
+def to_number(s: str) -> Optional[int]:
+    try:
+        return int(s.strip())
+    except ValueError:
+        return None
+
+
 def wrap_remaining(self: Reviewer, _old: Callable[[Reviewer], str]):
     if config["hide_card_type"] is True:
         html = _old(self)
         html = strip_html_tags(html)
-        numbers = html.split('+')
-        result = sum(int(n.strip()) for n in numbers)
-        return f'<span class="total-count">Left: {result}</span>'
+        numbers = (n for split in html.split('+') if (n := to_number(split)) is not None)
+        return f'<span class="total-count">Left: {sum(numbers)}</span>'
     else:
         return _old(self)
 
