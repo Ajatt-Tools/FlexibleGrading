f8e2cfa17269a848968adb5eeb4e83e360e43ef7
Ren Tatsumoto <tatsu@autistici.org>
2024-05-11 08:11:25 +0300

    call package.sh from ajt common

--- package.sh
+++ package.sh
@@ -1,18 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-readonly ADDON_NAME=flexible_grading
-readonly ROOT_DIR=$(git rev-parse --show-toplevel)
-readonly BRANCH=${1:-$(git branch --show-current)}
-readonly ZIP_NAME=${ADDON_NAME}_${BRANCH}.ankiaddon
-
-cd -- "$ROOT_DIR" || exit 1
-
-export ROOT_DIR BRANCH
-
-git archive "$BRANCH" --format=zip --output "$ZIP_NAME"
-
-# shellcheck disable=SC2016
-git submodule foreach 'git archive main --prefix=$path/ --format=zip --output "$ROOT_DIR/${path}_${BRANCH}.zip"'
-
-zipmerge "$ZIP_NAME" ./*.zip
-rm -- ./*.zip
+./ajt_common/package.sh \
+	--package "AJT Flexible Grading" \
+	--name "AJT Flexible Grading" \
+	--root "" \
+	"$@"
