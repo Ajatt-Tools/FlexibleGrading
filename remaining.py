f74f88347b6ddc8823cf4f254cd3bb58dfd7db27
Ren Tatsumoto <tatsu@autistici.org>
2024-09-18 03:50:47 +0300

    extract function format_remaining_cards()

--- remaining.py
+++ remaining.py
@@ -4,10 +4,12 @@
 import re
 from typing import Callable, Optional
 
+from anki.collection import Collection
+from anki.consts import REVLOG_RESCHED
 from anki.hooks import wrap
 from aqt.reviewer import Reviewer
 
-from .config import config, RemainingCountType
+from .config import RemainingCountType, config
 
 HTML_TAG = re.compile(r"<[^<>]+>", flags=re.IGNORECASE | re.MULTILINE)
 
@@ -27,13 +29,17 @@ def sum_remaining(html: str) -> int:
     return sum(n for split in strip_html_tags(html).split("+") if (n := to_number(split)) is not None)
 
 
-def wrap_remaining(self: Reviewer, _old: Callable[[Reviewer], str]) -> str:
+def format_remaining_cards(self: Reviewer, get_default_html: Callable[[Reviewer], str]):
     if config.remaining_count_type == RemainingCountType.none:
         return ""
     elif config.remaining_count_type == RemainingCountType.single:
-        return f'<span class="total-count">Left: {sum_remaining(_old(self))}</span>'
+        return f'<span class="ajt__total-count">Left: {sum_remaining(get_default_html(self))}</span>'
     else:
-        return _old(self)
+        return get_default_html(self).strip()
+
+
+def wrap_remaining(self: Reviewer, _old: Callable[[Reviewer], str]) -> str:
+    return format_remaining_cards(self, _old)
 
 
 def init():
