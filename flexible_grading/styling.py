ad3cf84617cf615ea589106146729e67b02f52ed
Ren Tatsumoto <tatsu@autistici.org>
2025-10-26 04:14:02 +0300

    make sure the reviewer css isn't lost

--- flexible_grading/styling.py
+++ flexible_grading/styling.py
@@ -1,10 +1,17 @@
 # Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-from typing import Any, Optional
+import pathlib
+from typing import Any, Final, Optional
 
 from aqt import gui_hooks, mw
 from aqt.reviewer import ReviewerBottomBar
 from aqt.webview import WebContent
+
+REVIEWER_CSS_PATH: Final[pathlib.Path] = pathlib.Path(__file__).parent / "web/ajt__reviewer.css"
+
+
+# Ensure everything is ok
+assert REVIEWER_CSS_PATH.is_file(), "reviewer CSS must exist"
 
 
 def on_webview_will_set_content(web_content: WebContent, context: Optional[Any]) -> None:
